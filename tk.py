83d7c629056a63947407f0ebe60f175e5de6250b
thekillerregs <thekillerregs@gmail.com>
2023-12-20 00:22:55 -0300

    Dictionaries (actually hashmaps)

--- tk.py
+++ tk.py
@@ -1,6 +1,24 @@
-# boolean
+# dictionary
+# come on... its called hashmap
 
-x = 10
-z = 20
+D = dict()
 
-print(x < z and 1 + 1 == 2)
+D['name'] = 'Tk'
+
+print(D['name'])
+
+print(D.__contains__('name'))
+
+intone = int(input('Enter number 1'))
+inttwo = int(input('Enter number 2'))
+
+product = intone * inttwo
+
+newDict = dict()
+
+newDict['firstNumber'] = intone
+newDict['secondNumber'] = inttwo
+newDict['product'] = product
+
+
+print(newDict.items())
