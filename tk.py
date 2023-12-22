66fc716b2f8b505fd2a969d4d0979f116b3b4ab3
thekillerregs <thekillerregs@gmail.com>
2023-12-22 12:14:30 -0300

    Classes and Object Oriented Programming

--- tk.py
+++ tk.py
@@ -1,16 +1,27 @@
-# copies and references
-import copy
+# OOP :D :D :D :D
 
-a = [4, 3]
+class model(object):
 
-b = a[:]
+    # constructor
+    def __init__(self, numlayers, numunits, name):
+        self.layers = numlayers  # attributes
+        self.units = numunits
+        self.name = name
+        self.weights = 1
 
-b[0] = 5
+    def howManyUnits(self):
+        totalUnits = self.layers * self.units
+        print(f'There are {totalUnits} units in the model')
 
-c = copy.deepcopy(b)
+    def train(self, x):
+        self.weights += x
+        return self.weights
 
-c[0] = 27
+    def __str_(self):
+        return self.name
 
-print(a)
-print(b)
-print(c)
+m1 = model(2, 5, 'TkModel')
+
+m1.howManyUnits()
+
+print(m1.train(2.5))
