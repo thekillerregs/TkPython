63fd42d5242df94c0039560fd77fb3b1469c369b
thekillerregs <thekillerregs@gmail.com>
2023-12-24 01:46:36 -0300

    variables

--- tk.py
+++ tk.py
@@ -1,7 +1,17 @@
-# continue
+# variables
 
-for i in range(10):
-    if i % 2 == 0:
-        continue
+import numpy as np
 
-    print(i)
+N = 10
+
+r = np.zeros(N)
+
+for i in range(N):
+    r[i] = i ** 2
+
+print(r)
+
+del r
+
+print(r)
+
