58f58fa1d71f0ce3279eebad62f443784eaa8fd1
thekillerregs <thekillerregs@gmail.com>
2023-12-24 01:18:04 -0300

    if else

--- tk.py
+++ tk.py
@@ -1,27 +1,9 @@
-# OOP :D :D :D :D
+# if
 
-class model(object):
+x = 1
 
-    # constructor
-    def __init__(self, numlayers, numunits, name):
-        self.layers = numlayers  # attributes
-        self.units = numunits
-        self.name = name
-        self.weights = 1
+if x == 1:
+    print("x is 1")
 
-    def howManyUnits(self):
-        totalUnits = self.layers * self.units
-        print(f'There are {totalUnits} units in the model')
-
-    def train(self, x):
-        self.weights += x
-        return self.weights
-
-    def __str_(self):
-        return self.name
-
-m1 = model(2, 5, 'TkModel')
-
-m1.howManyUnits()
-
-print(m1.train(2.5))
+else:
+    print("x is not 1")
