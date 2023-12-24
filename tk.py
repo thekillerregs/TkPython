3c6b384c30299bca08144c1c2ca4839a456eb169
thekillerregs <thekillerregs@gmail.com>
2023-12-24 15:50:52 -0300

    more graphs

--- tk.py
+++ tk.py
@@ -1,22 +1,44 @@
-# subplots
+# grafs
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.subplot(1, 2, 1)
-plt.plot(np.random.randn(10))
+x = np.array([1, 2, 3, 4, 5])
 
-plt.subplot(1, 2, 2)
-plt.plot(np.random.randn(10))
+plt.xlabel('fuck around')
+plt.ylabel('find out')
+
+plt.plot(x, x)
 
 plt.show()
 
+x = np.linspace(-3, 3, 101)
 
-fig, ax = plt.subplots(1,2, figsize=(10,4))
+plt.plot(x, x, label='y=x')
+plt.plot(x, x ** 2, label='y=x**2')
+plt.plot(x, x ** 3, label='y=x**3')
 
-x = np.arange(10)
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y=f(x)')
+plt.title('Mitada...')
 
-ax[0].plot(x,x**2, 'b')
-ax[1].plot(x,np.sqrt(x),'r')
+plt.xlim(x[0], x[-1])
+plt.ylim(-10, 10)
+
+plt.gca().set_aspect(.3)
+
+plt.show()
+
+fig, ax = plt.subplots(1)
+
+ax.plot(x, x, label='y=x')
+ax.plot(x, x ** 2, label='y=x**2')
+ax.plot(x, x ** 3, label='y=x**3')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y=f(x)')
+ax.set_title('Mitada²')
+
 
 plt.show()
