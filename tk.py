3eca165073299529a9f16007eea693ab2c31c8a0
thekillerregs <thekillerregs@gmail.com>
2023-12-24 16:35:37 -0300

    Image resolution and Vectors

--- tk.py
+++ tk.py
@@ -1,44 +1,17 @@
-# images
+# resolution
 
 import matplotlib.pyplot as plt
 import numpy as np
-from imageio import imread
-import seaborn as sns
+from IPython import display
+#deprecated :skull:
+display.set_matplotlib_formats('svg')
 
-m = 3
-n = 5
+x = np.linspace(.5,5,10)
+y1 = np.log(x)
+y2 = 2-np.sqrt(x)
 
-M = np.random.randint(10, size=(m, n))
-print(M)
+plt.plot(x,y1,'bo-', label='log')
+plt.plot(x,y2,'rs-',label='sqrt')
+plt.legend()
 
-plt.imshow(M)
-
-for i in range(m):
-    for j in range(n):
-        plt.text(j, i, str(M[i, j]), fontsize=10, horizontalalignment='center', verticalalignment='center')
-
-plt.colorbar()
-plt.show()
-
-img = imread(
-    'https://scontent.fpfb7-1.fna.fbcdn.net/v/t39.30808-6/297192965_575940604212380_2796628958234673299_n.jpg?stp=dst-jpg_p600x600&_nc_cat=108&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=5i7agVNth5kAX-B7t4Q&_nc_ht=scontent.fpfb7-1.fna&oh=00_AfByTpnplGwyDqZmWofRoaMMv8bSt8wmceaD0G4-2qIfGA&oe=658DF6CD')
-
-plt.imshow(img)
-plt.title('Sakayanagi')
-plt.show()
-
-plt.hist(img.flatten(), bins=100)
-plt.show()
-
-plt.imshow(img, cmap='gray', vmin=150, vmax=200)
-plt.xticks([])
-plt.yticks([])
-plt.show()
-
-hilbertMatrix = np.zeros([10, 10])
-for i in range(10):
-    for j in range(10):
-        hilbertMatrix[i, j] = 1 / (i + j + 1)
-
-sns.heatmap(hilbertMatrix, vmax=.4)
 plt.show()
