fb95bd19a2bbdd6abf439d2bf8b95627b726a81f
thekillerregs <thekillerregs@gmail.com>
2023-12-24 02:10:17 -0300

    broadcasting

--- tk.py
+++ tk.py
@@ -1,10 +1,27 @@
-# while
+# numpy broadcasting
 
-i = 0
+import numpy as np
 
+X = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]])
 
-while True:
-    print(i)
-    i += 1
+w = np.array([10, 20, 30])
 
-    if i == 8: break
+print(X), print(' '), print(w)
+
+Z = X + w
+
+print(Z), print(' '), print(Z)
+
+v = np.array([-1, 0, 1, 0], ndmin=2).T
+
+print(X), print(" "), print(X * v)
+
+tk = np.arange(9)
+
+M = np.reshape(tk, (3, 3))
+
+C = np.tile(M, (3, 1))
+
+B = C * np.reshape(tk, (-1, 1))
+
+print(tk), print(M), print(C), print(B)
