ccf966f0864cdd460db06a9d456613565234ff3d
thekillerregs <thekillerregs@gmail.com>
2023-12-24 02:13:43 -0300

    Throwing excepctions

--- tk.py
+++ tk.py
@@ -1,27 +1,11 @@
-# numpy broadcasting
+# exceptions
 
-import numpy as np
+def funfun(x, y):
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        raise Exception('Only numbers!')
 
-X = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]])
+    z = x * y
+    return z
 
-w = np.array([10, 20, 30])
 
-print(X), print(' '), print(w)
-
-Z = X + w
-
-print(Z), print(' '), print(Z)
-
-v = np.array([-1, 0, 1, 0], ndmin=2).T
-
-print(X), print(" "), print(X * v)
-
-tk = np.arange(9)
-
-M = np.reshape(tk, (3, 3))
-
-C = np.tile(M, (3, 1))
-
-B = C * np.reshape(tk, (-1, 1))
-
-print(tk), print(M), print(C), print(B)
+print(funfun(2, 2.5))
